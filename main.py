9ba927439886953dd6a8b4e87a0ba09a27d72d42
Josh <jkqpalermo@gmail.com>
2020-03-14 20:23:07 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import schedule
+import time
+from xml.etree import ElementTree
+import requests
+
+#apikey: JNTQivSRb4qxRVbQc5gHCB56M
+#victor'slocation: Latitude: 41.980262, Longitude: -87.668452
+
+url = "http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22"
+
+def pullRequests():
+    global busid, direction, latitude, longitude
+    tree = requests.get(url)
+    root = ElementTree.fromstring(tree.content)
+
+    for bus in root.findall('bus'):
+        busid = bus.find("id").text
+        direction = bus.find("dd").text
+        latitude = bus.find("lat").text
+
+        print("Bus ID: " + busid, "Direction: " + direction, "Latitude: " + latitude)
+
+
+schedule.every(10).seconds.do(pullRequests)
+
+while True:
+    schedule.run_pending()
