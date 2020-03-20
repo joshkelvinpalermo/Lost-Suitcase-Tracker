ac4598bca48d8aaafb3d2bd812ab137638eb1d07
Josh <jkqpalermo@gmail.com>
2020-03-20 18:45:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from pytz import timezone
 from xml.etree import ElementTree
 
 #apikey: JNTQivSRb4qxRVbQc5gHCB56M
-#victorslocation: Latitude: 41.980262, Longitude: -87.668452
 
 victorsLocation = (41.980262, -87.668452)
 tz = timezone('EST')
@@ -23,15 +22,16 @@ def pullRequests():
         latitude = bus.find('lat').text
         longitude = bus.find('lon').text
         direction = bus.find('dd').text
+        ws = '     '
 
         coordinates = latitude, longitude
 
-        print("ID: " + busid,
-              "Direction: " + direction,
-              ("{0:.2f}".format(distance.distance(victorsLocation, coordinates).km)))
+        print("Bus ID: " + busid, ws,
+              "Direction: " + direction, ws,
+              "Distance from Victor: " + ("{0:.2f}".format(distance.distance(victorsLocation, coordinates).km)))
 
 
-schedule.every(2).seconds.do(pullRequests)
+schedule.every(10).seconds.do(pullRequests)
 
 while True:
     schedule.run_pending()
