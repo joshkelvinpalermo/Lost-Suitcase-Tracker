d4b72d0a06053372f79d68a7afff66bb6e233ec1
Josh <jkqpalermo@gmail.com>
2020-03-20 14:01:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import schedule
-import time
+from datetime import datetime
+from pytz import timezone
 from xml.etree import ElementTree
 import requests
 
 #apikey: JNTQivSRb4qxRVbQc5gHCB56M
 #victorslocation: Latitude: 41.980262, Longitude: -87.668452
 
+tz = timezone('EST')
+esttime = datetime.now(tz)
 url = "http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22"
 
+
 def pullRequests():
-    global busid, direction, latitude, longitude
+    global busid, direction, latitude
     tree = requests.get(url)
     root = ElementTree.fromstring(tree.content)
 
@@ -18,10 +22,10 @@ def pullRequests():
         direction = bus.find("dd").text
         latitude = bus.find("lat").text
 
-        print("Bus ID: " + busid, "Direction: " + direction, "Latitude: " + latitude)
+        print("Bus ID: " + busid, "Latitude: " + latitude)
 
 
-schedule.every(10).seconds.do(pullRequests)
+schedule.every(3).seconds.do(pullRequests)
 
 while True:
     schedule.run_pending()
