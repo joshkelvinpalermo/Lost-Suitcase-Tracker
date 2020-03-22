c566526bba408918e2d1d0a43e63719689c06031
Josh <jkqpalermo@gmail.com>
2020-03-23 06:12:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import schedule
 import requests
-import tkinter
-from tkinter import messagebox
+import folium
+import branca
+import numpy
 from geopy import distance
 from datetime import datetime
 from pytz import timezone
 from xml.etree import ElementTree
 
-#CTAapikey: JNTQivSRb4qxRVbQc5gHCB56M
+#CTABusTrackerAPIKey: JNTQivSRb4qxRVbQc5gHCB56M
+#GoogleMapsAPIKey: AIzaSyACQPRJD-2M6kk_6RfUWRUYwOrsFAdZMS8
 
 victorsLocation = (41.980262, -87.668452)
 tz = timezone('EST')
 esttime = datetime.now(tz)
 url = "http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22"
+
+folium_map = folium.Map(location=[41.881832, -87.623177],
+                        zoom_start=10)
+marker = folium.CircleMarker(location=(41.881432, -87.627177))
+marker.add_to(folium_map)
+
+folium_map.save("my_map.html")
 
 def pullRequests():
     global busid, direction, latitude, longitude
@@ -31,9 +40,6 @@ def pullRequests():
         print("Bus ID: " + busid, ws,
               "Direction: " + direction, ws,
               "Distance from Victor: " + dist + "km")
-
-        if dist >= str(1):
-            messagebox.showinfo("There is a nearby bus!")
 
 
 schedule.every(5).seconds.do(pullRequests)
